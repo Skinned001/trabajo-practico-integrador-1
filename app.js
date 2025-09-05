f76dd35926c9226b709a4e06c7a86574f3a62db7
Laprida <psmathe001@gmail.com>
2025-09-05 11:25:02 -0300

    modelos importados en app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import { connectDB } from './src/config/database.js';
+
+import cookieParser from 'cookie-parser';
+import { UserModel } from './src/models/user.model.js';
+import { TagModel } from './src/models/tag.model.js';
+import { ProfileModel } from './src/models/profile.model.js';
+import { ArticleModel } from './src/models/article.model.js';
+import { ArticleTagModel } from './src/models/articleTag.model.js';
+
 
 
 const app = express();
