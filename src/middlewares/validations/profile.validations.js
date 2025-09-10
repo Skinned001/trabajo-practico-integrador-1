ed61f6c3b4729564eea78f65fafa600dc336cbb3
Laprida <psmathe001@gmail.com>
2025-09-10 16:41:33 -0300

    avances en las otros 3 archivos de validaciones restantes, base y referencias escritas

---
+++ src/middlewares/validations/profile.validations.js
@@ -0,0 +1,19 @@
+import { ProfileModel } from "../../models/profile.model";
+import { body,param } from "express-validator";
+import { Op } from "sequelize";
+
+export const createProfileValidation = [
+
+];
+
+export const getProfileById = [
+
+];
+
+export const updateProfile = [
+
+];
+
+export const deleteProfile = [
+
+];
---
+++ src/middlewares/validations/tag.validations.js
@@ -0,0 +1,19 @@
+import { tagModel } from "../../models/tag.model.js";
+import { body, param } from "express-validator";
+import { Op } from "sequelize";
+
+export const createTagValidation = [
+
+];
+
+export const updateTagValidation = [
+
+];
+
+export const getTagById = [
+
+];
+
+export const deleteTagValidation = [
+
+];
