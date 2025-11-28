b823ccd84213cd18550e8d0e21971fd0eaef96a4
Fernando <psamathe001@gmail.com>
2025-11-28 06:46:06 -0300

    corregidos errores de sintaxis y rutas

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,23 @@
+
 import { verifyToken } from "../helpers/jwt.helper.js";
 
 export const authMiddleware = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     if (!token) {
+      // 1. Token Faltante (No autenticado)
       return res.status(401).json({ message: "Usuario no autenticado" });
     }
-    const decoded = verifyToken(token);
+
+    const decoded = verifyToken(token); // Si falla, lanza un error
     req.userLogged = decoded;
-    console.log(decoded)
+    console.log(decoded);
     next();
   } catch (error) {
-    res.status(500).json({ message: "Error interno en el servidor" });
+
+    console.error("Error al verificar el token:", error.message);
+    return res.status(403).json({
+      message: "Acceso denegado: Token inválido o expirado.",
+    });
   }
 };
